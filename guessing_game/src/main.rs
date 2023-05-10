640fd8c6f9d4cf6b193691c5af32fba7aef98541
Victor Poglioni <victorrenop16@gmail.com>
2023-05-09 22:55:50 -0300

    Finished the guessing game with error handling

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -13,7 +13,10 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read the user input line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         
         println!("You guessed: {guess}");
         
