7be49fa27c3c818d65611d151da3f6e00e97be72
Victor Poglioni <victorrenop16@gmail.com>
2023-05-09 22:49:29 -0300

    Improve guessing game logic by adding a loop for guesses, stop on right guess and tips for wrong guesses

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,18 +1,29 @@
 use rand::Rng;
-use std::io;
+use std::{io, cmp::Ordering};
 
 fn main() {
     println!("Guess the number between 1 and 100 (and maybe win a prize)!");
-    println!("Please input your guess:");
-
-    let mut guess = String::new();
-
     let secret_number = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("Please input your guess:");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read the user input line");
-    
-    println!("You guessed: {guess}");
-    println!("The universe guessed: {secret_number}");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read the user input line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        
+        println!("You guessed: {guess}");
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("I expected more from you..."),
+            Ordering::Greater => println!("Lower your expectations please"),
+            Ordering::Equal => {
+                println!("You are correct... for now");
+                break;
+            },
+        }
+    }
 }
